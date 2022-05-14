6296a0701d5dc0c3c44501d98d0ea781fdc30c62
sk2111 <sathisht1996@gmail.com>
2022-05-14 17:09:08 +0530

    feat(containers): :sparkles: add user table with delete action

---
+++ src/components/containers/UsersTable/UsersTable.js
@@ -0,0 +1,59 @@
+//libs
+import React from "react";
+import PropTypes from "prop-types";
+//css
+import styles from "./UsersTable.module.css";
+//components
+import CheckBox from "components/reusables/CheckBox/CheckBox";
+//assests
+import editImgSrc from "assests/edit.png";
+import deleteImgSrc from "assests/delete.png";
+
+const UsersTable = ({ users, handleDelete }) => {
+  return (
+    <table className={styles.table}>
+      <thead>
+        <tr>
+          <th className={styles.checkboxCol}>
+            <CheckBox />
+          </th>
+          <th className={styles.nameCol}>Name</th>
+          <th className={styles.emailCol}>Email</th>
+          <th className={styles.roleCol}>Role</th>
+          <th className={styles.actionCol}>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.ids.map((id) => {
+          const user = users.entities[id];
+          return (
+            <tr key={id}>
+              <td>
+                <CheckBox />
+              </td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.role}</td>
+              <td>
+                <img className={styles.action} src={editImgSrc} alt="edit" />
+                <img
+                  className={styles.action}
+                  src={deleteImgSrc}
+                  alt="delete"
+                  onClick={() => handleDelete(id)}
+                />
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+UsersTable.propTypes = {
+  users: PropTypes.object.isRequired,
+  handleDelete: PropTypes.func.isRequired,
+};
+
+export default UsersTable;
