80a92129341e87fc6d1503ed759739e7939a97e3
sk2111 <sathisht1996@gmail.com>
2022-05-15 17:51:08 +0530

    test(containers): :white_check_mark: add users view unit test case

---
+++ src/components/containers/UsersView/UsersView.spec.js
@@ -0,0 +1,96 @@
+//libs
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import { Provider } from "react-redux";
+import userEvent from "@testing-library/user-event";
+import { configureStore } from "@reduxjs/toolkit";
+//component
+import UsersView from "./UsersView";
+//redux
+import userReducer, { fetchUsers } from "redux/userSlice";
+import { userActions } from "redux/userSlice";
+import { act } from "react-dom/test-utils";
+//mock
+const getMockFetchUsers = () => {
+  return [
+    {
+      id: 1,
+      name: "Aaron Miles",
+      email: "aaron@mailinator.com",
+      role: "member",
+    },
+    {
+      id: 2,
+      name: "Aishwarya Naik",
+      email: "aishwarya@mailinator.com",
+      role: "member",
+    },
+  ];
+};
+
+describe("UsersView", () => {
+  let store;
+
+  beforeEach(async () => {
+    store = configureStore({
+      reducer: {
+        user: userReducer,
+      },
+    });
+    jest.spyOn(window, "fetch").mockImplementationOnce(() => {
+      return {
+        json: jest.fn().mockResolvedValue(getMockFetchUsers()),
+      };
+    });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should render the users table & fire selection action", async () => {
+    const searchTerm = "admin";
+
+    render(
+      <Provider store={store}>
+        <UsersView />
+      </Provider>,
+    );
+
+    await userEvent.type(
+      screen.getByPlaceholderText("Search by name, email or role"),
+      searchTerm,
+    );
+
+    await waitFor(
+      function () {
+        const { user } = store.getState();
+        return expect(user.searchTerm).toEqual(searchTerm);
+      },
+      {
+        timeout: 2000,
+      },
+    );
+  });
+
+  it("should reset the pagination to total pages", async () => {
+    render(
+      <Provider store={store}>
+        <UsersView />
+      </Provider>,
+    );
+
+    await act(async () => {
+      await store.dispatch(fetchUsers());
+      await store.dispatch(userActions.updateCurrentPage(3));
+    });
+
+    await waitFor(
+      function () {
+        const { user } = store.getState();
+        return expect(user.currentPage).toBe(1);
+      },
+      { timeout: 2000 },
+    );
+  });
+});
